9179b29e5d19cc7228a1c352e8e19b1b5beaa1ac
WyrnCael <juanjpri@ucm.es>
2017-04-04 14:33:28 +0200

    Nodos con positivos y negativos

--- id3/src/estructuras/Nodo.java
+++ id3/src/estructuras/Nodo.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 
 public class Nodo {
 	private ArrayList<Nodo> hijos;
+	private int positivos;
+	private int negativos;
 	
 	public Nodo(){
 		hijos = new ArrayList<Nodo>();
+		this.positivos = 0;
+		this.negativos = 0;
 	}
 
 	public ArrayList<Nodo> getHijos() {
@@ -19,5 +23,29 @@ public class Nodo {
 	
 	public void addHijo(Nodo hijo){
 		this.hijos.add(hijo);
+	}
+
+	public int getPositivos() {
+		return positivos;
+	}
+
+	public void setPositivos(int positivos) {
+		this.positivos = positivos;
+	}
+	
+	public void addPositivo(){
+		this.positivos++;
+	}
+
+	public int getNegativos() {
+		return negativos;
+	}
+
+	public void setNegativos(int negativos) {
+		this.negativos = negativos;
+	}
+	
+	public void addNegativo(){
+		this.negativos++;
 	}
 }
--- id3/src/id3/Algoritmo.java
+++ id3/src/id3/Algoritmo.java
@@ -1,5 +1,20 @@
 package id3;
 
+import datos.Datos;
+import estructuras.Atributo;
+import estructuras.Nodo;
+
 public class Algoritmo {
 
+	public Algoritmo(){
+		
+	}
+	
+	public void getAlgorythm(){
+		// Contar positivos y negativos de cada atributo
+		for(Atributo atributo: Datos.getAtributos()){
+			Nodo arbol = new Nodo();
+			
+		}
+	}
 }
